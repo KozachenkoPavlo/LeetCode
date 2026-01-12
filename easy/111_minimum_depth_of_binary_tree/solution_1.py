195bafb870f4c87e32c671f97858d65183ec504e
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-01-12 22:03:28 +0100

    feature: 111

---
+++ easy/111_minimum_depth_of_binary_tree/solution_1.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        stack = [root]
+        depth = 0
+
+        while stack:
+            working_stack = stack.copy()
+            stack.clear()
+
+            while working_stack:
+                node = working_stack.pop(0)
+
+                if not node:
+                    continue
+
+                if node.left is None and node.right is None:
+                    return depth + 1
+                else:
+                    stack.append(node.left)
+                    stack.append(node.right)
+
+            depth += 1
+
+        return depth
