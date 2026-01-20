26c11a1465a6aeaf8646489d1bebe44ca30752c2
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-01-20 22:26:02 +0100

    feature: 563

---
+++ easy/563_binary_tree_tilt/solution_1.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        result = 0
+
+        def get_sum(node: Optional[TreeNode]) -> int:
+            if not node:
+                return 0
+
+            return node.val + get_sum(node.left) + get_sum(node.right)
+
+        def dfs(node: TreeNode):
+            nonlocal result
+
+            if node.left:
+                dfs(node.left)
+            if node.right:
+                dfs(node.right)
+
+            if node.left or node.right:
+                result += abs(get_sum(node.left) - get_sum(node.right))
+
+        dfs(root)
+
+        return result
