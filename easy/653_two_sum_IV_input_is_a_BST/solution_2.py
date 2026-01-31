edec13fab78cff757aadc9064558af0c57abbb5c
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-01-31 22:29:33 +0100

    todo: 653

---
+++ easy/653_two_sum_IV_input_is_a_BST/solution_2.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        # TODO: Optimal solution
+        pass
