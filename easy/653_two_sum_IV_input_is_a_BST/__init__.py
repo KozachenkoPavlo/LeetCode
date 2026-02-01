37a9d181c0783c313e0013a200fed7592d505d22
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-02-01 19:11:32 +0100

    choir: 653

--- easy/653_two_sum_IV_input_is_a_BST/__init__.py
+++ easy/653_two_sum_IV_input_is_a_BST/__init__.py
@@ -2,8 +2,8 @@ tags = [
     "Hash Table",
     "Two Pointers",
     "Tree",
-    "Depth - First Search",
-    "Breadth - First Search",
+    "Depth-First Search",
+    "Breadth-First Search",
     "Binary Search Tree",
     "Binary Tree",
 ]
