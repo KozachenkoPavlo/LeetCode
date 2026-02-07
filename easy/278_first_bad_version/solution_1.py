3432bfa3220d124dbc699b41dbb2144f065cdfc4
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-02-07 10:31:56 +0100

    feature: 278

---
+++ easy/278_first_bad_version/solution_1.py
@@ -0,0 +1,20 @@
+BAD_VERSION = 4
+
+
+def isBadVersion(version: int) -> bool:
+    return version < BAD_VERSION
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+
+        while left < right:
+            cursor = (left + right) // 2
+
+            if isBadVersion(cursor):
+                right = cursor
+            else:
+                left = cursor + 1
+
+        return right
