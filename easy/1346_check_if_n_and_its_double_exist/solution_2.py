5ef7a41cdb8a86fe9fe131fb2c8f66cfb955cc8b
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-02-12 17:31:22 +0100

    feature: 1346 Optimal solution

---
+++ easy/1346_check_if_n_and_its_double_exist/solution_2.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        seen = set()
+
+        for i in arr:
+            if i * 2 in seen or i / 2 in seen:
+                return True
+            seen.add(i)
+
+        return False
