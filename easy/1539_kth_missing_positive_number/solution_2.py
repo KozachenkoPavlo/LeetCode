588a58302ca7d01cd01cc05492748ae7929438b7
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-02-16 21:01:36 +0100

    todo: 1539 Optimal solution

--- easy/1539_kth_missing_positive_number/solution_2.py
+++ easy/1539_kth_missing_positive_number/solution_2.py
@@ -3,6 +3,15 @@ from typing import List
 
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        # TODO: Optimal solution
-        # Binary search
-        pass
+        left, right = 0, len(arr) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            missed_nums = arr[mid] - (mid + 1)
+
+            if missed_nums < k:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return k + left
