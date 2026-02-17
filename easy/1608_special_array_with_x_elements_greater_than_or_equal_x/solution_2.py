1fcc40ec11268da58415311f7a44b6b65e2fd9c6
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-02-17 21:47:36 +0100

    feature: 1608 Python tools

---
+++ easy/1608_special_array_with_x_elements_greater_than_or_equal_x/solution_2.py
@@ -0,0 +1,13 @@
+import bisect
+from typing import List
+
+
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort()
+
+        for i in range(len(nums) + 1):
+            if i == len(nums) - bisect.bisect_left(nums, i):
+                return i
+
+        return -1
