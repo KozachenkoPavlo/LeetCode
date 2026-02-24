3cdf9700e9a828e3791b03c9bf6bc5a3f773c4a2
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-02-24 22:07:34 +0100

    todo: 2970

---
+++ easy/2970_count_the_number_of_incremovable_subarrays_i/solution_2.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def incremovableSubarrayCount(self, nums: List[int]) -> int:
+        # TODO: optimal solution
+        # Take a look on this example(len, answer):
+        # [] 0, 1
+        # [1] 1, 1
+        # [1, 2] 2, 3
+        # [1, 2, 3] 3, 6
+        # [1, 2, 3, 4] 4, 10
+        # [1, 2, 3, 4, 5] 5, 15
+        # [1, 2, 3, 4, 5, 6] 6, 21
+        # [1, 2, 3, 4, 5, 6, 7] 7, 28
+        pass
