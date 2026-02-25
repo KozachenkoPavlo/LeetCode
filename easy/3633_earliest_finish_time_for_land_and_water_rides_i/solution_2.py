d8fcc7e95bc363ad4ad8e821ec7a9c8453ff9339
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-02-25 17:47:51 +0100

    todo: 3633

---
+++ easy/3633_earliest_finish_time_for_land_and_water_rides_i/solution_2.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def earliestFinishTime(
+            self,
+            landStartTime: List[int],
+            landDuration: List[int],
+            waterStartTime: List[int],
+            waterDuration: List[int]
+    ) -> int:
+        # TODO: Optimal solution
+        pass
