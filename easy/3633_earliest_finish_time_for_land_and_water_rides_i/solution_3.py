3020899f24b9dc9923ed7864afad89691bd30fc6
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-02-26 21:50:50 +0100

    feature: 3633 Optimal solution

--- easy/3633_earliest_finish_time_for_land_and_water_rides_i/solution_3.py
+++ easy/3633_earliest_finish_time_for_land_and_water_rides_i/solution_3.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from math import inf
+
 
 class Solution:
     def earliestFinishTime(
@@ -9,5 +11,21 @@ class Solution:
             waterStartTime: List[int],
             waterDuration: List[int]
     ) -> int:
-        # TODO: Optimal solution
-        pass
+        result = inf
+
+        def find_fastest(start_time_1: List[int], duration_1: List[int], start_time_2: List[int],
+                         duration_2: List[int]) -> int:
+            finish_1 = inf
+            for st, d in zip(start_time_1, duration_1):
+                finish_1 = min(finish_1, st + d)
+
+            finish_2 = inf
+            for st, d in zip(start_time_2, duration_2):
+                finish_2 = min(finish_2, max(finish_1, st) + d)
+
+            return finish_2
+
+        return min(
+            find_fastest(landStartTime, landDuration, waterStartTime, waterDuration),
+            find_fastest(waterStartTime, waterDuration, landStartTime, landDuration),
+        )
