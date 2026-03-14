d4996d5218e012697505dc5a6f36c2e280067e71
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-03-14 20:23:38 +0100

    refactor: 42 Improve speed

--- hard/42_trapping_rain_water/solution_1.py
+++ hard/42_trapping_rain_water/solution_1.py
@@ -7,21 +7,19 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         result = 0
 
-        max_lefts = [0]
-        max_rights = [0]
+        max_lefts = [0] * (len(height) + 1)
+        max_rights = [0] * (len(height) + 1)
 
         max_left = 0
         max_right = 0
 
         for i in range(len(height)):
             max_left = max(max_left, height[i])
-            max_lefts.append(max_left)
+            max_lefts[i + 1] = max_left
 
         for i in range(len(height) - 1, -1, -1):
             max_right = max(max_right, height[i])
-            max_rights.append(max_right)
-
-        max_rights.reverse()
+            max_rights[i - 1] = max_right
 
         for i in range(len(height)):
             r = min(max_lefts[i], max_rights[i]) - height[i]
