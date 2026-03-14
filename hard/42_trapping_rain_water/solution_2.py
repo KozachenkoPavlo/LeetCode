10e3a834398397dac2440ac39e267f8f881b4c6e
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-03-14 18:27:21 +0100

    refactor: 42 Save only indexes

--- hard/42_trapping_rain_water/solution_2.py
+++ hard/42_trapping_rain_water/solution_2.py
@@ -7,14 +7,14 @@ class Solution:
         stack = []
 
         for i in range(len(height)):
-            while len(stack) > 0 and stack[-1][1] < height[i]:
-                result += min(stack[0][1], height[i]) - stack[-1][1]
+            while len(stack) > 0 and height[stack[-1]] < height[i]:
+                result += min(height[stack[0]], height[i]) - height[stack[-1]]
 
                 if len(stack) > 1:
-                    result += (stack[-1][0] - stack[-2][0] - 1) * (min(height[i], stack[0][1]) - stack[-1][1])
+                    result += (stack[-1] - stack[-2] - 1) * (min(height[i], height[stack[0]]) - height[stack[-1]])
 
                 stack.pop()
 
-            stack.append((i, height[i]))
+            stack.append(i)
 
         return result
