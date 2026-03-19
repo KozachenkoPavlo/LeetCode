50d1299687866673bf722599076f5697efe34800
Kozachenko <96164075+KozachenkoPavlo@users.noreply.github.com>
2026-03-19 22:44:07 +0100

    refactor: 121 Optimal solution

--- medium/121_best_time_to_buy_and_sell_stock/solution_2.py
+++ medium/121_best_time_to_buy_and_sell_stock/solution_2.py
@@ -3,23 +3,15 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
+        l, r = 0, 0
         result = 0
 
-        while len(prices) > r > l:
-            if r < len(prices) - 1 and prices[r] < prices[r + 1]:
-                r += 1
-                continue
+        while r < len(prices) - 1:
+            r += 1
 
-            if l < len(prices) - 1 and prices[l] > prices[l + 1] and l < r - 1:
-                l += 1
-                continue
-
-            result = max(result, prices[r] - prices[l])
-
-            if len(prices) - 1 > r > l:
-                r += 1
+            if prices[l] < prices[r]:
+                result = max(result, prices[r] - prices[l])
             else:
-                l += 1
+                l = r
 
         return result
