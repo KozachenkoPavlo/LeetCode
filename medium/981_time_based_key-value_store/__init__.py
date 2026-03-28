e35a46d7655a4efcdeb7b64713e1869cc2d9f9c5
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-03-28 17:14:22 +0100

    feature: 981 Brute force solution

---
+++ medium/981_time_based_key-value_store/__init__.py
@@ -0,0 +1,8 @@
+tags = [
+    "Staff",
+    "Hash Table",
+    "String",
+    "Binary Search",
+    "Design",
+    "Weekly Contest 121",
+]
---
+++ medium/981_time_based_key-value_store/solution_1.py
@@ -0,0 +1,29 @@
+class TimeMap:
+    def _find_floor_index(self, key: str, timestamp: int) -> int:
+        for index, value in enumerate(self.time_map[key]):
+            if value[0] > timestamp:
+                return index - 1
+
+        return len(self.time_map[key]) - 1
+
+    def __init__(self):
+        self.time_map: dict[str, list[tuple[int, str]]] = {}
+
+    # Time: O(1)
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.time_map.keys():
+            self.time_map[key].append((timestamp, value))
+        else:
+            self.time_map[key] = [(timestamp, value)]
+
+    # Time: O(N)
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.time_map:
+            index = self._find_floor_index(key, timestamp)
+
+            if index == -1:
+                return ""
+
+            return self.time_map[key][index][1]
+
+        return ""
