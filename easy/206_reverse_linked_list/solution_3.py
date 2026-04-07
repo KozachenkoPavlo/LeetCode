6fe7e86e4d5cab6c6da8261be4e47a40f29284b6
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-04-07 21:21:59 +0200

    feature: 206 Not my solution. Tail Recursion

---
+++ easy/206_reverse_linked_list/solution_3.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return f"Node(val={self.val}, next={self.next})"
+
+# Tail Recursion
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def recurse(node: Optional[ListNode], previous: Optional[ListNode] = None) -> Optional[ListNode]:
+            if node is None:
+                return previous
+
+            next_node = node.next
+            node.next = previous
+
+            return recurse(next_node, node)
+
+        result = recurse(head)
+
+        return result
