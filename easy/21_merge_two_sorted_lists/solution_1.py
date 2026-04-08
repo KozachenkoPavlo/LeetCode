69c079e3d7476919b6f57ff88e87ce1f2012cb57
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-04-08 18:27:37 +0200

    feature: 21 Brute force solution

---
+++ easy/21_merge_two_sorted_lists/solution_1.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        result = ListNode()
+        head = result
+
+        while list1 or list2:
+            if list2 is None or (list1 and list1.val < list2.val):
+                head.next = ListNode(val=list1.val)
+                list1 = list1.next
+            else:
+                head.next = ListNode(val=list2.val)
+                list2 = list2.next
+
+            head = head.next
+
+        return result.next
