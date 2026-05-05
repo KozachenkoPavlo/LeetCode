c41e47ea9b53bf5e6cfa4937e0c635b50b52122d
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-05-05 20:58:15 +0200

    feature: 169 Brute force solution

---
+++ easy/169_majority_element/__init__.py
@@ -0,0 +1,7 @@
+tags = [
+    "Array",
+    "Hash Table",
+    "Divide and Conquer",
+    "Sorting",
+    "Counting",
+]
---
+++ easy/169_majority_element/solution_1.py
@@ -0,0 +1,18 @@
+class Solution:
+    # Time: O(N)
+    # Space: O(N)
+    def majorityElement(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        registry = {}
+        half_count_nums = (len(nums) + 1) // 2
+
+        for num in nums:
+            if num in registry:
+                registry[num] += 1
+
+                if registry[num] >= half_count_nums:
+                    return num
+            else:
+                registry[num] = 1
