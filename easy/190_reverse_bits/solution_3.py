9ce903dcbe0107755aec9b551e11c262b7b9ea62
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-05-07 13:05:12 +0200

    refactor: 190. Simplify code

--- easy/190_reverse_bits/solution_3.py
+++ easy/190_reverse_bits/solution_3.py
@@ -1,12 +1,12 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        bits = 32
         result = 0
 
-        for i in range(bits - 1, -1, -1):
+        for i in range(32):
             result = result << 1
             if n & 1:
                 result += 1
+
             n = n >> 1
 
         return result
