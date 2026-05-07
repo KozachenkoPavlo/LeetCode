86f73cb73d40ad5351fe0a99ace7caa5afc726d9
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-05-07 13:09:25 +0200

    refactor: 190. Remove unnecessary condition

--- easy/190_reverse_bits/solution_3.py
+++ easy/190_reverse_bits/solution_3.py
@@ -3,10 +3,7 @@ class Solution:
         result = 0
 
         for i in range(32):
-            result = result << 1
-            if n & 1:
-                result += 1
-
+            result = (result << 1) | (n & 1)
             n = n >> 1
 
         return result
