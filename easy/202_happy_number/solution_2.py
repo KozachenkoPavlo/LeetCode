8789d61a8b8d27f5278162feccccfa08a2f09c0b
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-05-10 23:20:03 +0200

    todo: 202 Solve using Two pointers

---
+++ easy/202_happy_number/solution_2.py
@@ -0,0 +1,10 @@
+from typing import Generator
+
+
+class Solution:
+    # Time: O(N)
+    # Space: O(1)
+    def isHappy(self, n: int) -> bool:
+        # TODO: Solve this problem using Linked Lists.
+        # Fast and Slow pointers should help you to find a cycle
+        pass
