6ab64f2a61267b1fdc013539b175eee9fa75f367
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-05-11 18:56:57 +0200

    feature: 202 Solution using Two pointers

--- easy/202_happy_number/solution_2.py
+++ easy/202_happy_number/solution_2.py
@@ -1,10 +1,24 @@
-from typing import Generator
-
-
 class Solution:
-    # Time: O(N)
+    # Time: O(log N), don't really know why
     # Space: O(1)
     def isHappy(self, n: int) -> bool:
-        # TODO: Solve this problem using Linked Lists.
-        # Fast and Slow pointers should help you to find a cycle
-        pass
+        def get_next(number: int) -> int:
+            result = 0
+
+            while number:
+                number, digit = divmod(number, 10)
+                result += digit ** 2
+
+            return result
+
+        slow, fast = n, n
+
+        while True:
+            slow = get_next(slow)
+            fast = get_next(get_next(fast))
+
+            if slow == 1:
+                return True
+
+            if slow == fast:
+                return False
