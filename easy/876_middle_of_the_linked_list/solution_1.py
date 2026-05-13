56df4289709f548c9b1e31fb94acd02405f4fe08
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-05-13 19:16:08 +0200

    chore: 876 Add metrics

--- easy/876_middle_of_the_linked_list/solution_1.py
+++ easy/876_middle_of_the_linked_list/solution_1.py
@@ -4,6 +4,8 @@ from data_structures import ListNode
 
 
 class Solution:
+    # Time: O(N), the list will be gone through 1.5 times
+    # Space: O(1), only two pointers
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         slow = head
         fast = head
