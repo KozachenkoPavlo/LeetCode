73947d4c0ce7e2f23103e99d04f7a2d2b3538660
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-05-20 18:55:06 +0200

    feature: Add methods for ListNode data structure

--- data_structures/list_node.py
+++ data_structures/list_node.py
@@ -1,5 +1,32 @@
 # Definition for singly-linked list.
+from typing import List, Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+    @staticmethod
+    def list_to_list_node(array: Optional[List]) -> Optional["ListNode"]:
+        dummy_node = ListNode()
+        node = dummy_node
+
+        for element in array:
+            node.next = ListNode(val=element)
+            node = node.next
+
+        return dummy_node.next
+
+    @staticmethod
+    def list_node_to_list(node: Optional['ListNode']) -> list:
+        result = []
+
+        while node:
+            result.append(node.val)
+            node = node.next
+
+        return result
+
+    def __str__(self) -> str:
+        return " -> ".join([str(element) for element in ListNode.list_node_to_list(self)])
