f6ae8f198278a35f383ab6e4e83a9f1776f5bf94
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-05-24 17:40:55 +0200

    todo: Solve using two pointers

---
+++ easy/234_palindrome_linked_list/solution_2.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from data_structures import ListNode
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # TODO: Come up with a better solution
+        # Using slow and fast pointers
+        # Reversing the second part of the list
+        pass
