e1c98e333484da1ca591c04d5bfe2f004db05445
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-06-03 18:18:05 +0200

    todo: 25 Recursion solution

---
+++ hard/25_reverse_nodes_in_k-group/solution_2.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+from data_structures import ListNode
+
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # TODO: Solve using recursion
+        pass
