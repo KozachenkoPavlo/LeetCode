8781aada78b1ccce9bc4c2a9032e20e55a6b3a48
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-06-08 19:53:16 +0200

    refactor: 567 Twice increase performance

--- easy/567_permutation_in_string/solution_1.py
+++ easy/567_permutation_in_string/solution_1.py
@@ -2,12 +2,11 @@ from collections import Counter
 
 
 class Solution:
-    def is_permutable(self, s1: str, s2: str) -> bool:
-        return Counter(s1) == Counter(s2)
-
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_hash = Counter(s1)
+
         for p1, p2 in [(i, i + len(s1)) for i in range(len(s2))]:
-            if self.is_permutable(s1, s2[p1:p2]):
+            if s1_hash == Counter(s2[p1:p2]):
                 return True
 
         return False
