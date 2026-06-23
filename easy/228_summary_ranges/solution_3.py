bbb29e6c4e4800ec5c838fcfb57497fb25487ed0
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-06-23 20:36:36 +0200

    todo: 228 Solution two pointers

---
+++ easy/228_summary_ranges/solution_3.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    # Time: O(N)
+    # Space: O(1), don't count the result, since it is mandatory
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        # TODO: Solve this using strategy two pointers
+        pass
