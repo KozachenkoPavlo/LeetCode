17f0fc4b89e1bc55a56bafa68ff0a65e9f94b64c
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-06-26 17:38:19 +0200

    feature: 239 Brute force solution with custom decreasing monotonic stack

---
+++ hard/239_sliding_window_maximum/solution_2.py
@@ -0,0 +1,69 @@
+from typing import List
+
+
+class DecreasingMonotonicStack:
+    def __init__(self, init_list: list[int] | None = None):
+        self.stack = []
+
+        if init_list is None:
+            return
+
+        for element in init_list:
+            self.add(element)
+
+    def add(self, element: int):
+        while self.stack and self.stack[-1] < element:
+            self.stack.pop()
+
+        self.stack.append(element)
+
+    def remove(self, element: int):
+        if self.stack[0] == element:
+            del self.stack[0]
+
+    def get_biggest(self):
+        return self.stack[0]
+
+    def __str__(self):
+        return self.stack
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        p1, p2 = 0, k
+        result = []
+        deque = DecreasingMonotonicStack(nums[p1:p2])
+
+        while True:
+            result.append(deque.get_biggest())
+
+            if p2 >= len(nums):
+                break
+
+            deque.remove(nums[p1])
+            deque.add(nums[p2])
+
+            p1 += 1
+            p2 += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    s = Solution()
+    tests = [
+        {"args": ([1, 2, 1, 0, 4, 2, 6], 3), "result": [2, 2, 4, 4, 6]},
+        {"args": ([1], 1), "result": [1]}
+    ]
+
+    for test in tests:
+        result = s.maxSlidingWindow(*test["args"])
+
+        if result == test["result"]:
+            print("PASSED!")
+        else:
+            print(f"Result: {result}")
+            print(f"Expected: {test['result']}")
