e43373c27999f7dde71c035e9fcfea46bd676fc8
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-07-08 21:29:49 +0200

    refactor: 230 Make algorythm more secure and fast

--- medium/230_kth_smallest_element_in_a_bst/solution_1.py
+++ medium/230_kth_smallest_element_in_a_bst/solution_1.py
@@ -4,18 +4,31 @@ from data_structures import TreeNode
 
 
 class Solution:
+    # Time: O(N)
+    # Space: O(H), where H is a height of a tree
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        array = []
+        result: Optional[int] = None
 
-        def dfs(node: TreeNode):
+        def dfs(node: TreeNode) -> None:
+            nonlocal k, result
+
+            if result is not None:
+                return
+
             if node.left:
                 dfs(node.left)
 
-            array.append(node.val)
+            k -= 1
+            if k == 0:
+                result = node.val
+                return
 
             if node.right:
                 dfs(node.right)
 
         dfs(root)
 
-        return array[k - 1]
+        if result is not None:
+            return result
+
+        raise RuntimeError("K is bigger then the length of the node!")
