b17cb3ae4540296afbb5e3bdc7ed02ad8d6c9bac
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-07-10 00:12:11 +0200

    feature: 448 Optimal solution

---
+++ easy/448_find_all_numbers_disappeared_in_an_array/solution_2.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        result = []
+        unique_nums = set(nums)
+
+        for num in range(1, len(nums) + 1):
+            if num not in unique_nums:
+                result.append(num)
+
+        return result
