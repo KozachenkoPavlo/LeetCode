ca58d46ff597630a31aec1ce3c55d0ece99c7637
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-07-15 23:36:34 +0200

    refactor: 506 Create medals list

--- easy/506_relative_ranks/solution_1.py
+++ easy/506_relative_ranks/solution_1.py
@@ -5,15 +5,12 @@ class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
         length = len(score)
         result = [""] * length
+        medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
         sorted_score = sorted([(s, i) for i, s in enumerate(score)], reverse=True)
 
         for i in range(length):
-            if i == 0:
-                rank = "Gold Medal"
-            elif i == 1:
-                rank = "Silver Medal"
-            elif i == 2:
-                rank = "Bronze Medal"
+            if i < 3:
+                rank = medals[i]
             else:
                 rank = str(i + 1)
 
