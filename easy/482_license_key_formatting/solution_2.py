d9515fec4ed2b923a86a020def99a08ca1697b8f
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-07-26 21:19:57 +0200

    feature: 482 Readable solution

---
+++ easy/482_license_key_formatting/solution_2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        result = s.replace("-", "").upper()[::-1]
+        groups = []
+
+        for i in range(0, len(result), k):
+            groups.append(result[i:i + k])
+
+        return "-".join([group[::-1] for group in groups[::-1]])
