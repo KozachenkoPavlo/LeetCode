a5772f26b4453a74d03ef8bf48807de6de3dd50f
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-07-26 21:15:04 +0200

    todo: 230

---
+++ medium/230_kth_smallest_element_in_a_bst/solution_2.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from data_structures import TreeNode
+
+
+class Solution:
+    # Time:
+    # Space:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # TODO: Solve using stack and while loop
+        pass
