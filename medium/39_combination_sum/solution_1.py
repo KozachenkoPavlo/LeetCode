436a8c3c07a662009fd71bbe1bc29b10215690ec
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-08-10 18:34:05 +0200

    feature: 39

---
+++ medium/39_combination_sum/solution_1.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    # Time: O(N ** (target / min(nums))
+    # Formula: (Max amount of branches) ** (Max depth of the recursion)
+    # Space: O(target / min(nums))
+    # Max depth of the tree is our Space complexity (We don't count result as it is required by the problem).
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        result = []
+        temp_nums = []
+        temp_sum = 0
+
+        def backtrace(pointer: int = 0):
+            nonlocal temp_sum
+
+            if temp_sum > target:
+                return
+
+            if temp_sum == target:
+                result.append(temp_nums.copy())
+                return
+
+            for index in range(pointer, len(nums)):
+                num = nums[index]
+                temp_nums.append(num)
+                temp_sum += num
+
+                backtrace(index)
+
+                temp_nums.pop()
+                temp_sum -= num
+
+        backtrace()
+
+        return result
