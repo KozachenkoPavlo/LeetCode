d6c933732c110f1ef31dcece6ec7e243d749f541
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-08-11 21:02:03 +0200

    feature: 40

---
+++ medium/40_combination_sum_ii/solution_1.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        target_sum = 0
+        subset = []
+        last_pop = None
+
+        candidates.sort()
+
+        def dfs(index: int):
+            nonlocal target_sum, last_pop
+
+            if target_sum == target:
+                result.append(subset.copy())
+                return
+
+            for candidate_index in range(index, len(candidates)):
+                candidate = candidates[candidate_index]
+
+                if target_sum + candidate > target:
+                    break
+
+                if last_pop == candidate:
+                    continue
+
+                target_sum += candidate
+                subset.append(candidate)
+
+                dfs(candidate_index + 1)
+
+                target_sum -= candidate
+                last_pop = subset.pop()
+
+        dfs(0)
+
+        return result
