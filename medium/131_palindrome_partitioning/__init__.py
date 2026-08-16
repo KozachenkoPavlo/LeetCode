fc5cbe44cb46e7e61de7f128c8474748ceabae05
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-08-16 16:53:32 +0200

    feature: 131

---
+++ medium/131_palindrome_partitioning/__init__.py
@@ -0,0 +1,5 @@
+tags = [
+    "String",
+    "Dynamic Programming",
+    "Backtracking",
+]
---
+++ medium/131_palindrome_partitioning/solution_1.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        current = []
+
+        def is_palindrome(left: int, right: int) -> bool:
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+
+            return True
+
+        def backtrace(index: int):
+            if index >= len(s):
+                result.append(current.copy())
+                return
+
+            for i in range(index, len(s)):
+                if not is_palindrome(index, i):
+                    continue
+
+                current.append(s[index:i + 1])
+
+                backtrace(i + 1)
+
+                current.pop()
+
+        backtrace(0)
+
+        return result
