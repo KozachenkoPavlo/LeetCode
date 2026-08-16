1183b5de0ca01dc1b8147270759573e8164f758d
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-08-16 17:04:45 +0200

    feature: 131 Adding cache for is_palindrome function

---
+++ medium/131_palindrome_partitioning/solution_2.py
@@ -0,0 +1,43 @@
+from typing import List
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        current = []
+        palindrome_cache = {}
+
+        def is_palindrome(left: int, right: int) -> bool:
+            origin_left, origin_right = left, right
+
+            if (left, right) in palindrome_cache:
+                return palindrome_cache[(left, right)]
+
+            while left < right:
+                if s[left] != s[right]:
+                    palindrome_cache[(origin_left, origin_right)] = False
+                    return False
+                left += 1
+                right -= 1
+
+            palindrome_cache[(origin_left, origin_right)] = True
+            return True
+
+        def backtrace(index: int):
+            if index >= len(s):
+                result.append(current.copy())
+                return
+
+            for i in range(index, len(s)):
+                if not is_palindrome(index, i):
+                    continue
+
+                current.append(s[index:i + 1])
+
+                backtrace(i + 1)
+
+                current.pop()
+
+        backtrace(0)
+
+        return result
