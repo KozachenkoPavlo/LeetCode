12ba289134bb8d3ea589dfd95a04e67a9f3d28c0
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-08-16 18:43:54 +0200

    todo: 463

---
+++ easy/463_island_perimeter/solution_2.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    # Time: O(N * M), where N is a width, and M is a height
+    # Space: O(N * M), because of recursion in case the whole grid has only 1
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        # TODO: Solve this problem using mathematical approach
+        pass
