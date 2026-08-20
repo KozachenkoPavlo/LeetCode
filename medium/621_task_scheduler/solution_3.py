5d995093aa62367d7e0d226412e8b443237d133b
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-08-20 22:38:58 +0200

    todo: 621 Mathematical solution

---
+++ medium/621_task_scheduler/solution_3.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    # Time: O(L * U) -> O(L)
+    # Space: O(U) -> O(1)
+    # Where:
+    # L = len(tasks)
+    # N = n
+    # U = len(set(tasks))
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        # TODO: Solve using mathematical trick
+        pass
