dde5fe5abc0894ae1441884339f2e7e09d72b323
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-08-21 21:28:43 +0200

    feature: 621 Mathematical solution

--- medium/621_task_scheduler/solution_3.py
+++ medium/621_task_scheduler/solution_3.py
@@ -1,13 +1,18 @@
+from collections import Counter
 from typing import List
 
 
 class Solution:
-    # Time: O(L * U) -> O(L)
+    # Time: O(L + U) -> O(L)
     # Space: O(U) -> O(1)
     # Where:
     # L = len(tasks)
     # N = n
     # U = len(set(tasks))
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # TODO: Solve using mathematical trick
-        pass
+        counter = Counter(tasks)  # O(L)
+        max_frequency = max(counter.values())  # O(U)
+        max_count = list(counter.values()).count(max_frequency)  # O(2 * U)
+        max_length = (max_frequency - 1) * (n + 1) + max_count  # O(1)
+
+        return max(len(tasks), max_length)
