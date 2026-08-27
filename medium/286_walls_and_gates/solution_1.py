61ab3d2d09eea10db64e6d16fb0179b1f6553080
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-08-27 06:39:30 +0200

    refactor: 286 Optimization
    - Extract loop from searching loop
    - Remove set seen
    - Change value immediately

--- medium/286_walls_and_gates/solution_1.py
+++ medium/286_walls_and_gates/solution_1.py
@@ -6,7 +6,6 @@ class Solution:
     def islandsAndTreasure(self, grid: List[List[int]]) -> None:
         height = len(grid)
         width = len(grid[0])
-        seen = set()
         queue = deque([])
 
         for row in range(height):
@@ -14,22 +13,21 @@ class Solution:
                 if grid[row][col] == 0:
                     queue.append((row, col, 0))
 
-                    while queue:
-                        r, c, d = queue.popleft()
+        while queue:
+            r, c, d = queue.popleft()
 
-                        if grid[r][c] > d:
-                            grid[r][c] = d
-                        else:
-                            if (r, c) in seen:
-                                continue
+            if c + 1 < width and grid[r][c + 1] == 2147483647:
+                grid[r][c + 1] = min(d + 1, grid[r][c + 1])
+                queue.append((r, c + 1, d + 1))
 
-                        seen.add((r, c))
+            if r + 1 < height and grid[r + 1][c] == 2147483647:
+                grid[r + 1][c] = min(d + 1, grid[r + 1][c])
+                queue.append((r + 1, c, d + 1))
 
-                        if c + 1 < width and grid[r][c + 1] not in {0, -1}:
-                            queue.append((r, c + 1, d + 1))
-                        if r + 1 < height and grid[r + 1][c] not in {0, -1}:
-                            queue.append((r + 1, c, d + 1))
-                        if c > 0 and grid[r][c - 1] not in {0, -1}:
-                            queue.append((r, c - 1, d + 1))
-                        if r > 0 and grid[r - 1][c] not in {0, -1}:
-                            queue.append((r - 1, c, d + 1))
+            if c > 0 and grid[r][c - 1] == 2147483647:
+                grid[r][c - 1] = min(d + 1, grid[r][c - 1])
+                queue.append((r, c - 1, d + 1))
+
+            if r > 0 and grid[r - 1][c] == 2147483647:
+                grid[r - 1][c] = min(d + 1, grid[r - 1][c])
+                queue.append((r - 1, c, d + 1))
