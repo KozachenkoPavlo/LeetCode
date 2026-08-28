b28239092a2ff2b7f0d62b433eb569898f6a17c7
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-08-28 04:58:24 +0200

    todo: 994

---
+++ medium/994_rotting_oranges/solution_2.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        # TODO: Solve using counter for fruits, to avoid using last for loop
+        pass
