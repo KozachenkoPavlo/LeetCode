41cddedb2f5cacff9da48bdb6d4f3395511ffd81
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-08-30 08:48:08 +0200

    feature: 4031

---
+++ medium/4031_find_all_numbers_disappeared_in_an_array_ii/solution_2.py
@@ -0,0 +1,4 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: list[int], lower: int, upper: int) -> list[list[int]]:
+        # TODO: Find a better solution for this problem
+        pass
