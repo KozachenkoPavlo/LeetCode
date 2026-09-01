51797833ccf546b7e965a500d627276c3f19a607
Kozachenko <96164075+kozachenkopavlo@users.noreply.github.com>
2026-09-01 20:24:19 +0200

    todo: 210

---
+++ medium/210_course_schedule_ii/solution_2.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # TODO: Find a better solution
+        pass
